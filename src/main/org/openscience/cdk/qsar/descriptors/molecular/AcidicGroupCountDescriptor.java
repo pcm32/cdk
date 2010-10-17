3483b8989b20cf29718d4246331ab2be964530a6
Egon Willighagen <egonw@users.sourceforge.net>
2010-10-17 15:02:44 +0200

    Added source for SMARTS queries

--- src/main/org/openscience/cdk/qsar/descriptors/molecular/AcidicGroupCountDescriptor.java
+++ src/main/org/openscience/cdk/qsar/descriptors/molecular/AcidicGroupCountDescriptor.java
@@ -37,8 +37,9 @@ import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
 
 /**
  * Returns the number of acidic groups. The list of acidic groups is defined
- * by this SMARTS "$([O;H1]-[C,S,P]=O)", "$([*;-;!$(*~[*;+])])", 
- * "$([NH](S(=O)=O)C(F)(F)F)", and "$(n1nnnc1)".
+ * by these SMARTS "$([O;H1]-[C,S,P]=O)", "$([*;-;!$(*~[*;+])])", 
+ * "$([NH](S(=O)=O)C(F)(F)F)", and "$(n1nnnc1)" originally presented in
+ * JOELib {@cdk.cite WEGNER2006}.
  *
  * @author      egonw
  * @cdk.module  qsarmolecular
